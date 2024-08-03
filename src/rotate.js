0811ce57f4fd8e381037db68dc473cd99b9a4c58
sufi4yan <sufiyanmasood598@gmail.com>
2024-08-04 00:00:57 +0500

    New game

---
+++ src/rotate.js
@@ -0,0 +1,235 @@
+export function rotatearray(newarr, degrees){
+    
+    let arr = [
+        [],
+        [],
+        []
+    ]
+    let arr2 = [
+        [],
+        [],
+        []
+    ]
+
+    if (degrees === 180){
+        for (let j = 0; j < 3; j++){
+            for (let i = 2; i >= 0; i--){
+              arr[j].push(newarr[i][j])
+              
+            }
+        }
+        for (let j = 0; j < 3; j++){
+            for (let i = 2; i >= 0; i--){
+              arr2[j].push(arr[i][j])
+
+              
+            }
+        }
+        return arr2
+    }
+    else if(degrees === 90){
+        for (let j = 0; j < 3; j++){
+            for (let i = 2; i >= 0; i--){
+              arr[j].push(newarr[i][j])
+                
+            }
+        }
+        return arr
+}
+    else if(degrees === 270){
+        let  count = 0
+        for (let i = 2; i >= 0; i--){
+            for (let j = 0; j < 3; j++){
+          arr[count].push(newarr[j][i])
+        }
+        count++
+    }
+    return arr
+       
+    }
+    else{
+        return newarr
+    }
+    }
+export function checkIf8(arr){
+
+        let buffer = 0
+        for (let i = 0; i <= 8; i++){
+            let count = 0
+            for (let j = 0; j < arr.length; j++){
+                if (Number(arr[j]) === Number(i)){
+    
+    
+                    count += 1
+    
+                    if (count === 8){
+                        buffer = i
+    
+                    }
+                    
+    
+                }
+            }
+        }
+        function getbuffer(){
+            return buffer
+        }
+        return {getbuffer}
+       
+        
+    }
+
+
+
+
+
+
+
+
+    
+
+export function countOnes(array2D) {
+    let count = 0;
+    for (let i = 0; i < array2D.length; i++) {
+        for (let j = 0; j < array2D[i].length; j++) {
+            if (array2D[i][j] === 1) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+
+export function leftzeros(displayedshape){
+    let num = 0
+for (let i = 0; i < 3; i++){
+
+   
+    if (displayedshape[i][0] === 0){
+        num ++
+    }
+    if (num === 3){
+        console.log(`mistake`)
+        for (let i = 0; i < 3; i++){
+            let buffer = displayedshape[i][0]
+        displayedshape[i][0] = displayedshape[i][1]
+        displayedshape[i][1] = displayedshape[i][2]
+        displayedshape[i][2] = buffer
+    }
+  
+    }
+}
+}
+
+export function topzeros(displayedshape){
+    let num = 0
+    for (let i = 0; i < 3; i++){
+
+       
+        if (displayedshape[0][i] === 0){
+            num ++
+        }
+        if (num === 3){
+            console.log(`mistake`)
+            for (let i = 0; i < 3; i++){
+                let buffer = displayedshape[0][i]
+            displayedshape[0][i] = displayedshape[1][i]
+            displayedshape[1][i] = displayedshape[2][i]
+            displayedshape[2][i] = buffer
+    
+        }
+
+        
+        }
+    }
+}
+
+export function html(row, ids, displayedshape, filledarr){
+    let color = `black`
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        color = `darkgray`
+    }
+   displayedshape[Number(row)].forEach((num) => {
+
+       if (num === 1 && !filledarr.includes(Number(ids))){
+           document.getElementById(ids).style.backgroundColor = color
+
+           ids += 100
+          
+       }
+       else{
+           ids += 100
+       }
+   
+   
+})
+}
+export function htmlclick(row, ids, mode, displayedshape){
+    let color = `black`
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        color = `white`
+    }
+    displayedshape[row].forEach((num) => {
+        try {
+            if (num === 1){
+                document.getElementById(ids).style.backgroundColor = color
+                mode.push(ids)
+                ids += 100
+               
+            }
+            else{
+                ids+=100
+            }
+        } 
+        catch (error) {
+            document.getElementById(ids).style.background = `none`
+    
+        }
+        
+    })
+}
+export function binaryStringTo3x3Array(binaryStr) {
+    let array = [
+        [],
+        [],
+        []
+    ];
+    for (let i = 0; i < 3; i++) {
+        let row = [];
+        for (let j = 0; j < 3; j++) {
+            row[j] = parseInt(binaryStr[i * 3 + j]);
+        }
+        array[i] = row;
+    }
+    return array;
+}
+
+export let shapes = [
+    "111100000", // array2d
+    "111001000", // array2d2
+    "110110000", // array2d3
+    "111010000", // array2d4
+    "101010101", // array2d5
+    "111010010", // array2d6
+    "110110001", // array2d7
+    "011010011", // array2d8
+    "011011011", // array2d9
+    "111111111", // array2d10
+    "111000000", // array2d11
+    "110000000", // array2d12
+    "100000000",  // array2d13
+    "110000000", // array2d12 
+    "101010000",
+    "101010000",
+    "111100000", // array2d
+    "111001000", // array2d2
+    "110110000", // array2d3
+    "111010000", // array2d4
+    "111010010", // array2d6
+
+    "011010011", // array2d8
+    "111000000", // array2d11
+    "110000000", // array2d12
+    "100000000",  // array2d13
+];
---
+++ src/script2.js
@@ -0,0 +1,553 @@
+import { rotatearray, checkIf8, countOnes, topzeros, leftzeros, html, htmlclick, binaryStringTo3x3Array, shapes } from "./rotate.js";
+import anime from "../node_modules/animejs/lib/anime.es.js";
+
+
+
+const scorecard = score()
+
+let num = 0
+let filtered
+
+let shape = binaryStringTo3x3Array(randomfromarray(shapes))
+
+
+   
+function randomfromarray(arr){
+    return arr[Math.floor(Math.random() * arr.length)]
+}
+
+const rotationpossibles = [0, 90, 180, 270]
+const randomRotate = randomfromarray(rotationpossibles)
+//console.log(randomRotate)
+
+let displayedshape = rotatearray(shape, randomRotate)
+
+
+const randdiv = document.getElementById(`random`)
+
+function proj(row, ids){
+    
+    displayedshape[Number(row)].forEach((num) => {
+
+        if (num === 1 && !filledarr.includes(Number(ids))){
+            projrarr.push(ids)
+
+            ids += 100 //next box id
+           
+        }
+        else{
+            ids+=100 //skip to next box without registering a filled box
+        }})
+}
+
+
+
+function all(){
+    shape = binaryStringTo3x3Array(randomfromarray(shapes))
+    displayedshape = rotatearray(shape, randomRotate)
+
+    //To remove empty lines from the 3x3 matrix
+    leftzeros(displayedshape)
+    topzeros(displayedshape)
+    //doing it again to remove any additional blank lines
+    leftzeros(displayedshape)
+    topzeros(displayedshape)
+    let randarr = []
+
+    setTimeout(() => {
+            randdiv.innerHTML = ``
+        displayedshape[0].forEach((number) => {
+            if (number === 1) {
+                //console.log(`yes`)
+                randdiv.innerHTML += `<div class="how"></div>`
+            }
+            else{
+                randdiv.innerHTML += `<div></div>`
+            }
+        })
+        displayedshape[1].forEach((number) => {
+            if (number === 1) {
+                //console.log(`yes`)
+                randdiv.innerHTML += `<div class="how"></div>`
+            }
+            else{
+                randdiv.innerHTML += `<div></div>`
+            }
+        })
+        displayedshape[2].forEach((number) => {
+            if (number === 1) {
+                //console.log(`yes`)
+                randdiv.innerHTML += `<div class="how"></div>`
+            }
+            else{
+                randdiv.innerHTML += `<div></div>`
+            }
+        })
+    }, 200);
+
+
+    filtered = randarr.filter(num => num !=0)
+    let newarr = []
+    for (let i = 0; i < filtered.length; i++){
+        newarr.push(i)
+        //console.log(newarr)
+    }
+
+
+}
+const childs = randdiv.childNodes
+
+randdiv.addEventListener(`click`, () => {
+    tracking = true
+    
+    
+})
+
+all()
+
+
+
+let projection = true
+
+const container = document.getElementById(`cont`)
+
+const sq = container.childElementCount ** 0.5
+//console.log(`sq = `, sq)
+container.style.gridTemplateColumns = `repeat(${sq}, 40px)`
+container.style.gridTemplateRows = `repeat(${sq}, 40px)`
+let filledarr = []
+let projrarr = []
+
+
+let tracking = true
+setInterval(() => {
+
+if (tracking) {
+    childs.forEach((child) => {
+        if(child.className === `how`){
+            child.style.border = `2px solid blue`
+        }
+    })
+}
+else{
+    childs.forEach((child) => {
+        if(child.className === `how`){
+            child.style.border = `1px solid white`
+        }
+    })
+}
+
+}, 10);
+
+
+container.addEventListener(`mouseover`, (eve) => {
+   
+    try {
+        let unfit = false
+        let id = eve.target.id
+                let idsx = Number(id)
+                let idsy = Number(id) + 1
+                let idsz = Number(id) + 2
+
+       
+        
+
+        if (tracking){
+            proj(0, idsx)
+            proj(1, idsy)
+            proj(2, idsz)
+
+
+        if (projrarr.length !== countOnes(displayedshape)){
+            projection = false
+            projrarr = []
+            throw new Error(`hello`)
+        }
+        else{
+            projection = true
+        }
+    
+            if (!unfit){
+                document.querySelectorAll(`.boxes`).forEach((box) => {
+
+                    if (!filledarr.includes(Number(box.id))){
+
+                        box.style.background = `none`
+                    }
+                        
+                    
+                    
+                })
+
+
+
+                filledarr.forEach((num) => {
+                    if (id === num) {
+                        
+                        //console.log(`what`)
+                        throw new Error(`hello`)
+                    }
+                })
+                projrarr = []
+                html(0, idsx, displayedshape, filledarr)
+                html(1, idsy, displayedshape, filledarr)
+                html(2, idsz, displayedshape, filledarr)
+    
+            }
+        } 
+        
+        
+            }
+            catch (error) {
+                projection = false
+                document.querySelectorAll(`.boxes`).forEach((box) => {
+                    
+                    if (!filledarr.includes(Number(box.id))){
+        
+                        box.style.background = `none`
+                    }
+                
+                
+            })
+            }
+ 
+})
+container.addEventListener(`touchstart`, (eve) => {
+
+    try {
+        let unfit = false
+        let id = eve.target.id
+                let idsx = Number(id)
+                let idsy = Number(id) + 1
+                let idsz = Number(id) + 2
+
+       
+        
+
+        if (tracking){
+            proj(0, idsx)
+            proj(1, idsy)
+            proj(2, idsz)
+
+
+        if (projrarr.length !== countOnes(displayedshape)){
+            projection = false
+            projrarr = []
+            throw new Error(`hello`)
+        }
+        else{
+            projection = true
+        }
+    
+            if (!unfit){
+                document.querySelectorAll(`.boxes`).forEach((box) => {
+
+                    if (!filledarr.includes(Number(box.id))){
+
+                        box.style.background = `none`
+                    }
+                        
+                    
+                    
+                })
+
+
+
+                filledarr.forEach((num) => {
+                    if (id === num) {
+                        
+                        //console.log(`what`)
+                        throw new Error(`hello`)
+                    }
+                })
+                projrarr = []
+                html(0, idsx, displayedshape, filledarr)
+                html(1, idsy, displayedshape, filledarr)
+                html(2, idsz, displayedshape, filledarr)
+    
+            }
+        } 
+        
+        
+            }
+            catch (error) {
+                projection = false
+                document.querySelectorAll(`.boxes`).forEach((box) => {
+                    
+                    if (!filledarr.includes(Number(box.id))){
+        
+                        box.style.background = `none`
+                    }
+                
+                
+            })
+            }
+
+   
+ 
+})
+
+container.addEventListener(`mouseup`, ev => {
+    projrarr = []
+    if (tracking && projection){
+        
+        tracking = false
+        const targ = ev.target
+        
+    
+    
+    
+    
+    
+        let id = ev.target.id
+    
+        let idsx = Number(id)
+        let idsy = Number(id) + 1
+        let idsz = Number(id) + 2
+          
+        htmlclick(0, idsx, filledarr, displayedshape) //for row 1
+        htmlclick(1, idsy, filledarr, displayedshape) //for row 2
+        htmlclick(2, idsz, filledarr, displayedshape) //for row 3
+
+            
+        all()
+    }
+    
+
+        let lines = 0
+   function iswin(){
+    
+    let finarrx = new Set()
+  
+    filledarr.forEach((num) => {
+        finarrx.add(String(num).replace(`0`, ``))
+
+    })
+
+    let finisher = []
+
+        finarrx.forEach((num) => {
+                finisher.push(Number(String(num).slice(-1)))
+            })
+
+                const columns = checkIf8(finisher.sort())
+            const columnsToRemove = columns.getbuffer()
+            if (columnsToRemove){
+                lines ++
+                for (let i = 1; i < 9; i++){
+                    
+                    anime({
+                        targets: document.getElementById(`${i}0${columnsToRemove}`),
+                        scale: [1.5, 0],
+                        duration:500,
+                        easing: `easeInOutQuad`,
+                        complete: function(){anime({
+                            targets: document.getElementById(`${i}0${columnsToRemove}`),
+                            scale: [0, 1],
+                            duration: 10
+                        })}
+                    
+                    
+                    })
+                    setTimeout(() => {
+                            document.getElementById(`${i}0${columnsToRemove}`).style.background = `none`
+                    }, 1500);
+                    
+                    //
+                    filledarr = filledarr.filter(num => num !== Number(`${i}0${columnsToRemove}`))
+
+                     
+                 }
+                 
+                 iswin()
+
+            }
+        return lines
+   }
+
+    lines = iswin()
+   if (lines > 0){
+    scorecard.increment(lines)
+   }
+    
+    
+
+const interval = setInterval(() => {
+        if (num <= scorecard.getscore()){
+        let scoretext = document.getElementById(`score`)
+        scoretext.innerText = num
+        num ++
+    
+    
+}
+else{
+    clearInterval(interval)
+}
+}, 1);
+  
+
+
+
+
+
+
+   let loss = isloss()
+   if (loss){
+
+    document.getElementById(`losepopup`).style.transform = `scale(1)`
+    document.getElementById(`endscore`).innerText = `Score: ${scorecard.getscore()}`
+    document.getElementById(`retry`).addEventListener(`click`, () =>  location.reload())
+   }
+
+
+
+
+
+
+
+
+
+
+
+            
+            
+             
+
+})
+
+document.onreadystatechange = function() {
+
+    if (document.readyState !== "complete") {
+        
+        document.querySelector("#loader").style.visibility = "visible";
+    } else {
+        setTimeout(() => {
+            document.querySelector("#loader").style.display = "none";
+            document.querySelector("#cont").style.visibility = "visible";
+            document.querySelector("#generate").style.visibility = "visible";
+        }, 15);
+
+
+        
+    }
+};
+document.querySelector("#cont").style.visibility = "hidden";
+document.querySelector("#generate").style.visibility = "hidden";
+console.log(document.readyState)
+
+
+let children = Array.from(document.querySelector(`#cont`).children)
+let arrayn = []
+children.forEach((child) => {
+ if (!filledarr.includes(Number(child.id)))
+ arrayn.push(Number(child.id))
+
+})
+function isloss(){
+    let loss = false
+    let errors = 0
+    tracking = true
+    arrayn.forEach((i) => {
+        try {
+            let unfit = false
+            let id = i
+                    let idsx = Number(id)
+                    let idsy = Number(id) + 1
+                    let idsz = Number(id) + 2
+    
+            
+           
+            
+    
+            if (tracking){
+                proj(0, idsx)
+                proj(1, idsy)
+                proj(2, idsz)
+    
+    
+            if (projrarr.length !== countOnes(displayedshape)){
+                projection = false
+                projrarr = []
+                throw new Error(`hello`)
+            }
+            else{
+                projection = true
+            }
+        
+                if (!unfit){
+                    document.querySelectorAll(`.boxes`).forEach((box) => {
+    
+                        if (!filledarr.includes(Number(box.id))){
+    
+                            box.style.background = `none`
+                        }
+                            
+                        
+                        
+                    })
+    
+    
+    
+                    filledarr.forEach((num) => {
+                        if (id === num) {
+                            
+                            //console.log(`what`)
+                            throw new Error(`hello`)
+                        }
+                    })
+                    projrarr = []
+                    html(0, idsx, displayedshape, filledarr)
+                    html(1, idsy, displayedshape, filledarr)
+                    html(2, idsz, displayedshape, filledarr)
+        
+                }
+            } 
+            
+            
+                }
+                catch (error) {
+                    errors++
+                    
+                    projection = false
+                    document.querySelectorAll(`.boxes`).forEach((box) => {
+                        
+                        if (!filledarr.includes(Number(box.id))){
+            
+                            box.style.background = `none`
+                        }
+                    
+                    
+                })
+                }
+    })
+    console.log(errors)
+   if (errors === 64){
+    loss = true
+    return loss
+   }
+}
+
+let loss = isloss()
+if (loss){
+ setTimeout(() => {
+    location.reload()
+ }, 100);
+    
+}
+function score(){
+    let scores = 0
+
+    function increment(lines){
+        scores += (Math.round(Math.random() * 4 * 10) ** lines ) 
+    }
+
+    function getscore() {
+        return scores
+    }
+    return {increment, getscore}
+}
+
+
+
+
+    
